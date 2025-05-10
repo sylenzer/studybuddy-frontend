b578cb2abef0614174cd4377ebd8808cc9637ed1
sylenzer <34782383+sylenzer@users.noreply.github.com>
2025-05-10 17:44:49 +0000

    fix background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,10 +5,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div
-      className="min-h-screen flex flex-col bg-cover bg-center"
-      style={{ backgroundImage: "url('/converted_image_2025-05-10T16-12-31-679Z.png')" }}
-    >
+    <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-grow flex justify-center items-center px-4 py-8">
         {children}
